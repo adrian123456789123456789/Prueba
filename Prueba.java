41e3a6cc3c3823862586fe4f5b97e1da687bff01
adrian <adri.adrian15@gmail.com>
2018-02-26 13:55:07 +0100

    Intercambio inicial de proyecto

---
+++ Prueba.java
@@ -0,0 +1,28 @@
+
+/**
+ *  
+ * 
+ * @author  
+ * @version  
+ */
+public class Prueba
+{
+     
+    private int x;
+
+    /**
+     * Constructor  
+     */
+    public Prueba()
+    {
+         
+    }
+
+    /**
+     *  
+     */
+    public void metodo()
+    {
+         
+    }
+}
